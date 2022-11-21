af94dfc05be00cd7d70bb4995c2c2672faee198b
jaime <jaimeken@yahoo.es>
2022-11-21 19:15:36 +0100

    creamos rama txispis parano liarla en la principal

---
+++ Sociedades.java
@@ -0,0 +1,13 @@
+public class Sociedades {
+   package jaime.chispas;
+
+    public class sociedades {
+        private String CodEmpresa;
+        public String NombreEmpresa;
+        public String Poblacion;
+        Pulic String Provincia;
+        Public String
+        public Integer get ID() {return id;
+        public void set Nombre(String nombre){this.nombre=nombre:
+        }
+}
