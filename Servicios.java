5bcc5920346a883e6362dd300f667bc4a969fe91
jaime <jaimeken@yahoo.es>
2022-11-22 22:34:03 +0100

    creamos rama txispis parano liarla en la principal; impresio--<como invocar impresora?

---
+++ Servicios.java
@@ -0,0 +1,15 @@
+public static Servicios (){
+public class Servicios
+public Integer CodServicio;
+public Integer Precio;
+public Intege Nombre;
+Public IntegerIVA;
+private Integer TotalFactura;
+pivate Integer BasIponible;
+    get id Integer=this id:
+        set id integer=set id;
+
+
+        }
+{
+}
