065e86e8755176a0299d0b7939b7a8f213c32537
jaime <jaimeken@yahoo.es>
2022-11-22 22:33:38 +0100

    creamos rama txispis parano liarla en la principal; impresio--<como invocar impresora?

---
+++ Impresion.java
@@ -0,0 +1,16 @@
+public class Impresion {
+    public Integer NombreCliente;
+    public Direccion;
+    private Integer CodFactura;
+    public Integer direccionCliente;
+    public Integer poblacion
+            public Integer provincia;
+    Private Sericios;
+    get impresionInteger=this impresion;
+    set impresion Integer;
+    private timestamp;
+    private dia Integer de factura;
+    private invocar impresora;
+
+
+}
